fd3bd3cb57c4870019a01fe9e2f06a8a43628452
Aravinth <sptaravin-dev@gmail.com>
2024-07-24 16:39:38 +0800

    added installation of nvim and lazygit

--- install.sh
+++ install.sh
@@ -22,3 +22,13 @@ if ! chezmoi=$(command -v chezmoi); then
     sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
     unset chezmoi_install_script bin_dir
 fi
+
+echo "Installing nvim latest"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo tar -C /usr/local/ -xzf nvim-linux64.tar.gz --strip-components=1
+
+echo "Installing lazygit"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
