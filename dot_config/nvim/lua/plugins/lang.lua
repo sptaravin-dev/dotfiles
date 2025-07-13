11ee1af89457339675028cc1f7bd90ade338dba4
Aravinth Tamilarasan <sptaravin.dev@gmail.com>
2025-07-13 16:00:42 +0800

    changes to lsp structure

---
+++ dot_config/nvim/lua/plugins/lang.lua
@@ -0,0 +1,9 @@
+return {
+  -- Import all plugins from lang folder
+  { import = "plugins.lang.python" },
+  { import = "plugins.lang.go" },
+  { import = "plugins.lang.ansible" },
+  { import = "plugins.lang.terraform" },
+
+  -- other plugin specs here...
+}
