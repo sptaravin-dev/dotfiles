7e8d09f2a0fa3d9debef859b32ff37cb1341dda3
sptaravin-dev <sptaravin.dev@gmail.com>
2025-07-18 18:06:27 +0800

    add devcontainer setup

--- run_once_install_ansible.sh
+++ run_once_install_ansible.sh
@@ -3,15 +3,33 @@
 install_on_ubuntu() {
   sudo apt-get update
   sudo apt-get install -y ansible
-  ansible-galaxy install juju4.gpgkey_generate
   ansible-playbook ~/.bootstrap/setup-debian.yml --ask-become-pass -vv
 }
 
 install_on_mac() {
   brew install ansible
-  ansible-galaxy install juju4.gpgkey_generate
   ansible-playbook ~/.bootstrap/setup-macos.yml --ask-become-pass -vv
 }
+
+install_on_devcontainer {
+  sudo apt-get update
+  sudo apt-get install -y ansible
+  ansible-playbook ~/.bootstrap/setup-devcontainer.yml --ask-become-pass -vv
+}
+
+if [ -f "/.devcontainer.json" ] || [ "$DEVCONTAINER" = "true" ]; then
+  echo "Installing inside a DevContainer"
+
+  if grep -qi ubuntu /etc/os-release; then
+    echo "Confirmed: Ubuntu-based DevContainer"
+    install_on_devcontainer
+    echo "Ansible installation complete."
+    exit 0
+  else
+    echo "Non-Ubuntu DevContainer detected. Aborting."
+    exit 1
+  fi
+fi
 
 OS="$(uname -s)"
 case "${OS}" in
