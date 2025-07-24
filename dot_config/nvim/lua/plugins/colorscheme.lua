a6f007cccda5b7f9914e1ba7e74f6a1dd9e18c5b
sptaravin-dev <sptaravin.dev@gmail.com>
2025-07-24 22:13:15 +0800

    macos changes

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,18 @@
--- iceberg theme
 return {
-  -- Install iceberg
-  { "cocopon/iceberg.vim", lazy = true },
-  -- Tell LazyVim to pick iceberg as the default
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  { "cocopon/iceberg.vim" },
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "iceberg" },
+    opts = {
+      colorscheme = "kanso-ink",
+    },
   },
 }
-
--- gruvbox-material theme
--- return {
---   "sainnhe/gruvbox-material",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     -- Optionally configure and load the colorscheme
---     -- directly inside the plugin declaration.
---     vim.g.gruvbox_material_enable_italic = true
---     vim.cmd.colorscheme("gruvbox-material")
---   end,
--- }
