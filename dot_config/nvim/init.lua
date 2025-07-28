a877aa236f52a998d7507d46f48dea9ebb547fbe
sptaravin-dev <sptaravin.dev@gmail.com>
2025-07-28 18:08:05 +0800

    minor changes

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Enable true color support
+vim.opt.termguicolors = true
+
+-- Set cursor color in Normal mode
+vim.cmd([[highlight Cursor guifg=#14171d guibg=#e2a478]])
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -4,10 +4,13 @@ return {
     defaults = {
       layout_strategy = "vertical",
       layout_config = {
-        mirror = true, -- Preview above the results
-        preview_height = 0.6, -- Adjust to your liking
-        height = 0.9,
-        prompt_position = "bottom",
+        preview_height = 0.7,
+        vertical = {
+          size = {
+            width = "95%",
+            height = "95%",
+          },
+        },
       },
     },
   },
